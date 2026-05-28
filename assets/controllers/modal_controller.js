bf2f732c15cb8c5fdf3f7e29debfddbb54f8f5e0
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-05-28 13:17:55 +0000

    wip saving progress
    I need to eat and this modal doesn't want to collaborate

--- assets/controllers/modal_controller.js
+++ assets/controllers/modal_controller.js
@@ -3,19 +3,27 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     static targets = ['container'];
 
-    open() {
+    connect() {
+        console.log('[modal] connected', this.element);
+    }
+
+    open(event) {
+        console.log('[modal] open() fired', event.type, event.target);
+        event.stopPropagation();
         this.containerTarget.classList.remove('hidden');
         document.body.classList.add('overflow-hidden');
     }
 
-    close() {
+    close(event) {
+        console.log('[modal] close() fired');
+        if (event) event.stopPropagation();
         this.containerTarget.classList.add('hidden');
         document.body.classList.remove('overflow-hidden');
     }
 
     backdropClose(event) {
         if (event.target === this.containerTarget) {
-          this.close();
+            this.close(event);
         }
     }
 }
