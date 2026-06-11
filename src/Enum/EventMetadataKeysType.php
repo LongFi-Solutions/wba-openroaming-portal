62fa991a30fbba4841e529e02601795f1f6601ef
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-06-11 09:56:57 +0000

    fix enum

--- src/Enum/EventMetadataKeysType.php
+++ src/Enum/EventMetadataKeysType.php
@@ -4,9 +4,9 @@ namespace App\Enum;
 
 enum EventMetadataKeysType: string
 {
-    case IP = 'IP';
-    case USER_AGENT = 'USER_AGENT';
-    case PLATFORM = 'PLATFORM';
-    case UUID = 'UUID';
+    case IP = 'ip';
+    case USER_AGENT = 'user_agent';
+    case PLATFORM = 'platform';
+    case UUID = 'uuid';
 
 }
