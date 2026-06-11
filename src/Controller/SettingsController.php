280d2ee3dbe6f0aa3761737992a4d83176972531
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-06-11 15:05:24 +0000

    review events (SETTING_SMS_CONF_REQUEST)

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -1011,10 +1011,10 @@ class SettingsController extends AbstractController
                 AnalyticalEventType::SETTING_SMS_CONF_REQUEST->value,
                 new DateTime(),
                 [
-                    'ip' => $request->getClientIp(),
-                    'user_agent' => $request->headers->get('User-Agent'),
-                    'uuid' => $currentUser->getUuid(),
-                    'changeset'  => $changeset,
+                    EventMetadataKeysType::IP->value => $request->getClientIp(),
+                    EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                    EventMetadataKeysType::UUID->value => $currentUser->getUuid(),
+                    EventMetadataKeysType::CHANGESET->value  => $changeset,
                 ]
             );
 
