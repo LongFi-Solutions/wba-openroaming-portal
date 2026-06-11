b7fc6f28f4d524e5b01333d0b2b6a41028afc606
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-06-11 15:40:29 +0000

    AnalyticalEventType CERTIFICATE_SETUP_PROCESS_ABORTED

--- src/Controller/CertificateManagementController.php
+++ src/Controller/CertificateManagementController.php
@@ -264,9 +264,9 @@ class CertificateManagementController extends AbstractController
             AnalyticalEventType::CERTIFICATE_SETUP_PROCESS_ABORTED->value,
             new DateTime(),
             [
-                'ip' => $request->getClientIp(),
-                'user_agent' => $request->headers->get('User-Agent'),
-                'by' => $user->getUuid(),
+                EventMetadataKeysType::IP->value => $request->getClientIp(),
+                EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                EventMetadataKeysType::UUID->value => $user->getUuid(),
             ]
         );
 
