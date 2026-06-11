b2c0f315e3b2c39f2d8aff6a98a638cacc70e667
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-06-11 12:38:12 +0000

    review events (SETTING_LDAP_CONF_REQUEST)

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -644,11 +644,10 @@ class SettingsController extends AbstractController
                 AnalyticalEventType::SETTING_LDAP_CONF_REQUEST->value,
                 new DateTime(),
                 [
-                    'ip' => $request->getClientIp(),
-                    'user_agent' => $request->headers->get('User-Agent'),
-                    'uuid' => $currentUser->getUuid(),
-                    'platform' => $data[SettingName::PLATFORM_MODE->value]['value'],
-                    'changeset'  => $changeset,
+                    EventMetadataKeysType::IP->value => $request->getClientIp(),
+                    EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                    EventMetadataKeysType::UUID->value => $currentUser->getUuid(),
+                    EventMetadataKeysType::CHANGESET->value  => $changeset,
                 ]
             );
 
