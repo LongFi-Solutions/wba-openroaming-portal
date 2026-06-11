f2257cb8aee7ac10ed8e72be61980f759f86c21a
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-06-11 16:13:59 +0000

    AnalyticalEventType INSTALLATION_COMMAND_CONFIG

--- src/Controller/InstallationController.php
+++ src/Controller/InstallationController.php
@@ -287,9 +287,9 @@ class InstallationController extends AbstractController
                 AnalyticalEventType::INSTALLATION_COMMAND_CONFIG->value,
                 new DateTime(),
                 [
-                    'ip' => $request->getClientIp(),
-                    'user_agent' => $request->headers->get('User-Agent'),
-                    'by' => $user->getUuid(),
+                    EventMetadataKeysType::IP->value => $request->getClientIp(),
+                    EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                    EventMetadataKeysType::UUID->value => $user->getUuid(),
                 ]
             );
 
