ad7cfeb712082d0fde89afd619c4b9325fe54552
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-06-11 16:17:32 +0000

    AnalyticalEventType SYSTEM_RESET_REQUEST_STARTED

--- src/Controller/CertificateManagementController.php
+++ src/Controller/CertificateManagementController.php
@@ -327,9 +327,9 @@ class CertificateManagementController extends AbstractController
             AnalyticalEventType::SYSTEM_RESET_REQUEST_STARTED->value,
             new DateTime(),
             [
-                'ip' => $request->getClientIp(),
-                'user_agent' => $request->headers->get('User-Agent'),
-                'by' => $user->getUuid(),
+                EventMetadataKeysType::IP->value => $request->getClientIp(),
+                EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                EventMetadataKeysType::UUID->value => $user->getUuid(),
             ]
         );
 
