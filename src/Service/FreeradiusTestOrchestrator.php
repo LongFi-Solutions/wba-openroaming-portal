b0d8a291e6aae13b329a1541acd564922b64e26f
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-06-11 16:20:32 +0000

    AnalyticalEventType SYSTEM_RESET_REQUEST_COMPLETED

--- src/Service/FreeradiusTestOrchestrator.php
+++ src/Service/FreeradiusTestOrchestrator.php
@@ -59,9 +59,9 @@ final readonly class FreeradiusTestOrchestrator
                 AnalyticalEventType::SYSTEM_RESET_REQUEST_COMPLETED->value,
                 new DateTime(),
                 [
-                    'ip' => $request->getClientIp(),
-                    'user_agent' => $request->headers->get('User-Agent'),
-                    'by' => $user->getUuid(),
+                    EventMetadataKeysType::IP->value => $request->getClientIp(),
+                    EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                    EventMetadataKeysType::UUID->value => $user->getUuid(),
                 ]
             );
 
