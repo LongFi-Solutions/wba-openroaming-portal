cf623d85fbd4483757479db4eb245c2d190856e3
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-06-11 12:11:10 +0000

    review events (SETTING_PLATFORM_2FA_REQUEST)

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -797,10 +797,10 @@ class SettingsController extends AbstractController
                 AnalyticalEventType::SETTING_PLATFORM_2FA_REQUEST->value,
                 new DateTime(),
                 [
-                    'ip' => $request->getClientIp(),
-                    'user_agent' => $request->headers->get('User-Agent'),
-                    'uuid' => $currentUser->getUuid(),
-                    'changeset'  => $changeset,
+                    EventMetadataKeysType::IP->value => $request->getClientIp(),
+                    EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                    EventMetadataKeysType::UUID->value => $currentUser->getUuid(),
+                    EventMetadataKeysType::CHANGESET->value  => $changeset,
                 ]
             );
 
