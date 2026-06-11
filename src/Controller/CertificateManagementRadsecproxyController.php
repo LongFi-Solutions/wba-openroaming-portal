fd0618e75387937da52bd2977c15f595c67fec0a
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-06-11 15:42:58 +0000

    AnalyticalEventType CERTIFICATE_SETUP_PROCESS_RADSECPROXY_CONFIG

--- src/Controller/CertificateManagementRadsecproxyController.php
+++ src/Controller/CertificateManagementRadsecproxyController.php
@@ -236,9 +236,9 @@ class CertificateManagementRadsecproxyController extends AbstractController
                     AnalyticalEventType::CERTIFICATE_SETUP_PROCESS_RADSECPROXY_CONFIG->value,
                     new DateTime(),
                     [
-                        'ip' => $request->getClientIp(),
-                        'user_agent' => $request->headers->get('User-Agent'),
-                        'by' => $user->getUuid(),
+                        EventMetadataKeysType::IP->value => $request->getClientIp(),
+                        EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                        EventMetadataKeysType::UUID->value => $user->getUuid(),
                     ]
                 );
 
