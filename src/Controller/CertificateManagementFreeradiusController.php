0924b32a79a19f6950136c1f8b1bd40b9704f105
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-06-11 15:46:25 +0000

    AnalyticalEventType CERTIFICATE_SETUP_PROCESS_FREERADIUS_TEST

--- src/Controller/CertificateManagementFreeradiusController.php
+++ src/Controller/CertificateManagementFreeradiusController.php
@@ -14,6 +14,7 @@ use App\Enum\AnalyticalEventType;
 use App\Enum\CertificateFileName;
 use App\Enum\CertificateMachineType;
 use App\Enum\CertificateTestResult;
+use App\Enum\EventMetadataKeysType;
 use App\Enum\FirewallType;
 use App\Enum\ProcessStatusType;
 use App\Enum\SessionStatus;
@@ -112,7 +113,7 @@ class CertificateManagementFreeradiusController extends AbstractController
         $session = $request->getSession();
         $session->set(
             SessionStatus::FREERADIUS_SETUP_PROCESS_TYPE->value,
-            AnalyticalEventType::CERTIFICATE_SETUP_PROCESS_FREERAEDIUS_UPLOAD_MANUAL->value,
+            AnalyticalEventType::CERTIFICATE_SETUP_PROCESS_FREERADIUS_UPLOAD_MANUAL->value,
         );
 
         // Prepare DTO
@@ -226,12 +227,12 @@ class CertificateManagementFreeradiusController extends AbstractController
             $user = $this->getUser();
             $this->eventActions->saveEvent(
                 $user,
-                AnalyticalEventType::CERTIFICATE_SETUP_PROCESS_FREERAEDIUS_UPLOAD_MANUAL->value,
+                AnalyticalEventType::CERTIFICATE_SETUP_PROCESS_FREERADIUS_UPLOAD_MANUAL->value,
                 new DateTime(),
                 [
-                    'ip' => $request->getClientIp(),
-                    'user_agent' => $request->headers->get('User-Agent'),
-                    'by' => $user->getUuid(),
+                    EventMetadataKeysType::IP->value => $request->getClientIp(),
+                    EventMetadataKeysType::USER_AGENT->value => $request->headers->get('User-Agent'),
+                    EventMetadataKeysType::UUID->value => $user->getUuid(),
                 ]
             );
 
