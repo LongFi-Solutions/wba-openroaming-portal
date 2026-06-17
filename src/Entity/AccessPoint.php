ef8457d4c56f3751ee28afa1c07eaf0bcd77197b
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-06-17 11:39:01 +0000

    fix phpstan

--- src/Entity/AccessPoint.php
+++ src/Entity/AccessPoint.php
@@ -12,6 +12,7 @@ class AccessPoint
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    /** @phpstan-ignore-next-line */
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'accessPoints')]
