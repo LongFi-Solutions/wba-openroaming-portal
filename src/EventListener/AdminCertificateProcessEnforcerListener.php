f19b742a9447570fbc3975976edbfc05bc899d8d
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-06-18 09:45:12 +0000

    wip add missing ignore route for certs if they are not defined

--- src/EventListener/AdminCertificateProcessEnforcerListener.php
+++ src/EventListener/AdminCertificateProcessEnforcerListener.php
@@ -94,17 +94,19 @@ readonly class AdminCertificateProcessEnforcerListener
             '#^/dashboard/settings/certificatesManagement/freeradius/selection#',
 
             // Freeradius steps
-                // Auto Generation Lets Encrypt
+
+            // Auto Generation Lets Encrypt
             '#^/dashboard/settings/certificatesManagement/freeradius/autoRenew$#',
             '#^/dashboard/settings/certificatesManagement/freeradius/autoRenewDomain#',
-                // Manual Upload
+            // Manual Upload
             '#^/dashboard/settings/certificatesManagement/freeradius/upload$#',
             '#^/dashboard/settings/certificatesManagement/freeradius/config$#',
             '#^/dashboard/settings/certificatesManagement/freeradius/test$#',
             '#^/dashboard/settings/certificatesManagement/freeradius/test/run$#',
 
-                // Cloudflare
-            '#^/dashboard/settings/certificatesManagement/freeradius/cloudflare/dnsChallenge$#'
+            // Cloudflare
+            '#^/dashboard/settings/certificatesManagement/freeradius/cloudflare/dnsChallenge$#',
+            '#^/dashboard/settings/certificatesManagement/freeradius/cloudflare/httpChallenge#'
         ];
         $allowed = array_any(
             $allowedPatterns,
