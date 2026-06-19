67aad34d217da6fe94d8186df0aa203a9dbcd38e
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-06-19 12:36:36 +0000

    map permissions

--- src/DTO/UserAddDTO.php
+++ src/DTO/UserAddDTO.php
@@ -68,6 +68,8 @@ class UserAddDTO
     public PermissionLevel $smsConfig = PermissionLevel::NONE;
     public PermissionLevel $portalStatistics = PermissionLevel::NONE;
     public PermissionLevel $connectivityStatistics = PermissionLevel::NONE;
+    public PermissionLevel $activityLogs = PermissionLevel::NONE;
+    public PermissionLevel $map = PermissionLevel::NONE;
 
     /**
      * Returns AdminPermissionsType strings based on selected levels
@@ -92,6 +94,8 @@ class UserAddDTO
             'smsConfig' => 'SMS_CONFIG',
             'portalStatistics' => 'PORTAL_STATISTICS',
             'connectivityStatistics' => 'CONNECTIVITY_STATISTICS',
+            'activityLogs' => 'ACTIVITY_LOGS',
+            'map' => 'MAP'
         ];
 
         $permissions = [];
