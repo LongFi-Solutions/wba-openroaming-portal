99ee015851160a11a438a3de98df33a413f919e1
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-07-16 10:01:11 +0000

    wip

--- migrations/Version20260707165340.php
+++ migrations/Version20260707165340.php
@@ -20,13 +20,22 @@ final class Version20260707165340 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE AccessPoint CHANGE location location POINT DEFAULT NULL');
-        $this->addSql('ALTER TABLE Network CHANGE geometry geometry GEOMETRY SRID 4326 DEFAULT NULL');
+        $this->addSql("UPDATE AccessPoint SET location = ST_GeomFromText('POINT(0 0)') WHERE location IS NULL");
+        $this->addSql("UPDATE Network SET geometry = ST_GeomFromText('GEOMETRYCOLLECTION EMPTY', 4326) WHERE geometry IS NULL");
+
+        $this->addSql('ALTER TABLE AccessPoint CHANGE location location POINT NOT NULL');
+        $this->addSql('ALTER TABLE Network CHANGE geometry geometry GEOMETRY SRID 4326 NOT NULL');
+
+        $this->addSql('CREATE SPATIAL INDEX idx_ap_location ON AccessPoint (location)');
+        $this->addSql('CREATE SPATIAL INDEX idx_network_geometry ON Network (geometry)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX idx_ap_location ON AccessPoint');
+        $this->addSql('DROP INDEX idx_network_geometry ON Network');
+
         $this->addSql('ALTER TABLE AccessPoint CHANGE location location JSON DEFAULT NULL');
         $this->addSql('ALTER TABLE Network CHANGE geometry geometry JSON DEFAULT NULL');
     }
