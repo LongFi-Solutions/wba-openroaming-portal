32dfc1d376dae03e3653f8260962c6d31dc841f1
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-07-17 16:45:57 +0000

    wip -> command

---
+++ src/Command/MultipleSMSMigrationCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'MultipleSMSMigration',
+    description: 'Add a short description for your command',
+)]
+class MultipleSMSMigrationCommand extends Command
+{
+    public function __construct(
+
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('reset:allSettings')
+            ->setDescription('Reset All Settings')
+            ->addOption('yes', 'y', InputOption::VALUE_NONE, 'Automatically confirm the reset');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Check if the --yes option is provided (comes from a controller), then skip the confirmation prompt
+        if (!$input->getOption('yes')) {
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion('This action will migrate ALL SMS SETTINGS fot the new format. [y/N] ', false);
+            /** @var QuestionHelper $helper */
+            if (!$helper->ask($input, $output, $question)) {
+                $output->writeln('Command aborted.');
+                return Command::SUCCESS;
+            }
+        }
+
+
+        // get SMS_USERNAME, SMS_USER_ID, SMS_HANDLE and add to the new format
+
+
+        return Command::SUCCESS;
+    }
+}
