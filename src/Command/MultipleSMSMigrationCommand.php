4e6c61f351eb87c931cd61d3133bb09a43548d1a
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-07-20 09:07:04 +0000

    wip upgrade command tag and descriptions

--- src/Command/MultipleSMSMigrationCommand.php
+++ src/Command/MultipleSMSMigrationCommand.php
@@ -5,16 +5,14 @@ namespace App\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'MultipleSMSMigration',
-    description: 'Add a short description for your command',
+    name: 'prepare:multiSMSMigration',
+    description: 'Migrate current credentials about BudgetSMS Api from the Settings table for the new dedicated SMSProvider management',
 )]
 class MultipleSMSMigrationCommand extends Command
 {
@@ -28,9 +26,11 @@ class MultipleSMSMigrationCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setName('reset:allSettings')
-            ->setDescription('Reset All Settings')
-            ->addOption('yes', 'y', InputOption::VALUE_NONE, 'Automatically confirm the reset');
+            ->setName('prepare:multiSMSMigration')
+            ->setDescription(
+                'Migrate current credentials about BudgetSMS Api from the Settings table for the new dedicated SMSProvider management'
+            )
+            ->addOption('yes', 'y', InputOption::VALUE_NONE, 'Automatically confirm the migration');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -38,7 +38,10 @@ class MultipleSMSMigrationCommand extends Command
         // Check if the --yes option is provided (comes from a controller), then skip the confirmation prompt
         if (!$input->getOption('yes')) {
             $helper = $this->getHelper('question');
-            $question = new ConfirmationQuestion('This action will migrate ALL SMS SETTINGS fot the new format. [y/N] ', false);
+            $question = new ConfirmationQuestion(
+                'This action will migrate ALL SMS SETTINGS Budget Api details for the new format. [y/N] ',
+                false
+            );
             /** @var QuestionHelper $helper */
             if (!$helper->ask($input, $output, $question)) {
                 $output->writeln('Command aborted.');
