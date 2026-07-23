626fe93a41452ba77fc45e3108fcdbeab57472c7
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-07-23 17:16:22 +0000

    wip revert the reverted

--- src/Controller/MapAccessPointExportController.php
+++ src/Controller/MapAccessPointExportController.php
@@ -97,15 +97,25 @@ class MapAccessPointExportController extends AbstractController
                     fputcsv(
                         $handle,
                         [
-                            $ap->getName(),
-                            $ap->getSsid(),
-                            $ap->getMacAddress(),
-                            $ap->getVendor(),
-                            $ap->getModel(),
-                            $ap->getStandard(),
-                            $ap->getSerialNumber(),
-                            $lng !== '' ? number_format((float)$lng, 6, '.', '') : '',
-                            $lat !== '' ? number_format((float)$lat, 6, '.', '') : '',
+                            $this->sanitizeCsvField($ap->getName()),
+                            $this->sanitizeCsvField($ap->getSsid()),
+                            $this->sanitizeCsvField($ap->getMacAddress()),
+                            $this->sanitizeCsvField($ap->getVendor()),
+                            $this->sanitizeCsvField($ap->getModel()),
+                            $this->sanitizeCsvField($ap->getStandard()),
+                            $this->sanitizeCsvField($ap->getSerialNumber()),
+                            $lng !== '' ? number_format(
+                                (float)$lng,
+                                6,
+                                '.',
+                                ''
+                            ) : '',
+                            $lat !== '' ? number_format(
+                                (float)$lat,
+                                6,
+                                '.',
+                                ''
+                            ) : '',
                             $ap->getAltitudeMsl(),
                             $ap->getAltitudeAgl()
                         ],
